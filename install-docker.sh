b5a8a169da92a6513f955da54356b83293fb6650
David Carboni <david@carboni.io>
2019-02-01 12:37:39 -0500

    The commands needed to install Docker (edge version)

---
+++ install-docker.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# Update the apt package index:
+sudo apt-get update
+
+# Install packages to allow apt to use a repository over HTTPS:
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
+# Add Docker's official GPG key:
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+
+# Add the "edge" repository to work with this version of Ubuntu
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt update
+
+sudo apt install -y docker-ce docker-ce-cli containerd.io
+
