b039ab70952c182af833c978a53ea785c3a2f934
David Carboni <david@carboni.io>
2019-09-06 18:25:51 +0100

    Updated Compose version.

--- compose/install-docker-compose.sh
+++ compose/install-docker-compose.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
-sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+# See: https://docs.docker.com/compose/install/
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+
