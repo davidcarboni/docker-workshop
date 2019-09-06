51adc54e1008bdf9868133f0355cb0ebdab2856c
David Carboni <david@carboni.io>
2019-09-06 19:14:08 +0100

    Added some hints for things to try.

--- run-nginx.sh
+++ run-nginx.sh
@@ -8,4 +8,4 @@
 docker run -it --rm -p 80:80 nginx
 
 # Ctrl-C to quit the container
-# Try changing some of those options, e.g. take away --rm and then do a 'docker ps'
+# Try changing some of those options, e.g. take away --rm and then do a 'docker ps -a' after stopping the container to verify that it hasn't been automatically deleted, or use -d instead of -it to run the container in the background, detatched from the command line. Use `docker ps` to see the running container and `docker rm -f <name>` to delete it.
